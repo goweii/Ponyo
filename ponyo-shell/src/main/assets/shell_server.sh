add0c9ec98506cfd422adf9d989118fb77964d98
goweii <goweii@163.com>
2021-07-10 20:55:19 +0800

    新增shell

--- ponyo-shell/src/main/assets/shell_server.sh
+++ ponyo-shell/src/main/assets/shell_server.sh
@@ -1,1 +1,3 @@
+adb push shell_server /data/local/tmp
+adb shell rename /data/local/tmp/shell_server /data/local/tmp/shell_server.dex
 adb shell app_process -Djava.class.path=/data/local/tmp/shell_server.dex /system/bin --nice-name=ponyoshell per.goweii.ponyo.shell.server.ShellServer
